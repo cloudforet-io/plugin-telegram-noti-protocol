47577a276041c1fe937368e77276c4c4d43c2ab4
jean1042 <jean1042@naver.com>
2021-07-21 22:51:54 +0900

    Update telegram plugin service

--- src/spaceone/notification/service/notification_service.py
+++ src/spaceone/notification/service/notification_service.py
@@ -1,6 +1,9 @@
 import logging
+import telegram
 
+from telegram.ext import CommandHandler
 from spaceone.core.service import *
+from spaceone.notification.error import *
 from spaceone.notification.manager.notification_manager import NotificationManager
 
 _LOGGER = logging.getLogger(__name__)
@@ -19,16 +22,37 @@ class NotificationService(BaseService):
         Args:
             params:
                 - options
-                - message
-                - notification_type
+                - message :
+                - notification_type : INFO || ERROR || SUCCESS || WARNING
                 - secret_data
         """
         secret_data = params.get('secret_data', {})
         notification_type = params['notification_type']
         message = params['message']
 
-        telegram_token = secret_data.get('token')
-        channel = secret_data.get('channel')
+        telegram_token = secret_data.get('token')  # bot token
+        group_name = secret_data.get('group_name')
+        bot = telegram.Bot(telegram_token)
+        updates = bot.get_updates()
+        chat_id = self._get_dispatch_chat_id(updates=updates, group_name=group_name)
 
         noti_mgr: NotificationManager = self.locator.get_manager('NotificationManager')
-        noti_mgr.dispatch(telegram_token, channel, f'[{notification_type}] {message["message"]}')
+        noti_mgr.dispatch(token=telegram_token, chat_id=chat_id, message=message)
+
+    def _get_dispatch_chat_id(self, updates, group_name):
+        try:
+            for update in updates:
+                if update.my_chat_member:  # Update object's metadata class
+                    pass
+                else:
+                    chat_title = update.message.chat.title
+
+                    if str(chat_title) == group_name:
+                        chat_id = update.message.chat.id
+                        return chat_id
+
+        except Exception as e:
+            _LOGGER.error(f'[_get_dispatch_chat_id] Cannot find the chat id for the group name: {getattr(e, "message", e)}')
+            raise ValueError
+
+
