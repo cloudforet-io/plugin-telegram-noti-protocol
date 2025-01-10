3fb9c382d1203a6612c7bb5e2eaf6a5af1214046
ImMin5 <mino@megazone.com>
2025-01-10 11:52:13 +0900

    feat: modify send message when raised bad request error
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/spaceone/notification/connector/telegram_connector.py
+++ src/spaceone/notification/connector/telegram_connector.py
@@ -14,10 +14,17 @@ class TelegramConnector(BaseConnector):
         self.bot = telegram.Bot(token=kwargs.get('token'))
 
     async def send_message(self, chat_id, message, reply_markup=None, image_url=None):
-        if reply_markup:
-            await self.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup, parse_mode='HTML')
-        else:
-            await self.bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
-
-        if image_url:
-            await self.bot.send_photo(chat_id=chat_id, photo=image_url)
+        try:
+            if reply_markup:
+                await self.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup,
+                                            parse_mode='HTML')
+            else:
+                await self.bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
+            if image_url:
+                await self.bot.send_photo(chat_id=chat_id, photo=image_url)
+        except telegram.error.BadRequest as e:
+            _LOGGER.error(f'[send_message] Error: {e.message}', exc_info=True)
+            await self.bot.send_message(chat_id=chat_id, text=message)
+        except Exception as e:
+            _LOGGER.error(f'[send_message] Error: {e}', exc_info=True)
+            await self.bot.send_message(chat_id=chat_id, text=message)
